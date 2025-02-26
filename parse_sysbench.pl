08cbf0db457bf2270f502f5575e8c01d79b61292
Brian Misachi <misachibrian@gmail.com>
2025-02-26 22:15:56 +0300

    Update parser"s regular expression to work with my current version of sysbench output

--- parse_sysbench.pl
+++ parse_sysbench.pl
@@ -2,8 +2,8 @@
 
 my @files = `find . -name sysbench*`;
 
-open( OUTFILE, "> all_res.data" ) or die "could not open all_res.data: $!";
-print OUTFILE "run, time, tps, resp_time\n";
+open( OUTFILE, "> out/all_res.data" ) or die "could not open all_res.data: $!";
+print OUTFILE "run, time, tps\n";
 
 while( my $file = shift @files ) {
     next if $file =~ m/\.data$/;
@@ -17,11 +17,18 @@ while( my $file = shift @files ) {
     open( FILE, "< $file" ) or die "could not open $file: $!";
     
 
-    
+
     while( my $line = <FILE> ) {
         # print $line;
-        if( $line =~ m/^\[\s*(\d+)s\].+,\stps\:\s(\d+\.\d+).+,\sresponse\stime\:\s(\d+\.\d+)/ ) {
-            print OUTFILE "$run, $1, $2, $3\n";
+
+        # The regular expression does not work as set up before, for me
+        # This is a workaround to make it work for me: Pick the required fields one by one; and remove unknown fields(response time)
+        my $l1, $l2;
+        if( $line =~ m/^\[\s(\d+)s\s\].+/) {
+            $l1 = $1;
+            if ( $line =~ m/\stps\:\s(\d+\.\d+).+/ ) {
+                print OUTFILE "$run, $l1, $1\n";
+            }
         }
     } 
     close( FILE );
