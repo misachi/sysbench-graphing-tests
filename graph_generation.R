7172a5335fb56cd5d300d6cf6710d41153e220d0
Brian Misachi <misachibrian@gmail.com>
2025-02-26 22:17:18 +0300

    Ignore graph for response time: Field does not exist in sysbench
    Output to untracked directory

--- graph_generation.R
+++ graph_generation.R
@@ -1,7 +1,7 @@
 library( ggplot2 )
 
-all_res = read.csv( "all_res.data" )
-pdf( "result_graphs.pdf" )
+all_res = read.csv( "out/all_res.data" )
+pdf( "out/result_graphs2.pdf" )
 print( qplot( time, tps, color=run, data=all_res, xaxs="i" ))
-print( qplot( time, resp_time, color=run, data=all_res, ylim = c(0,500) ))
+# print( qplot( time, resp_time, color=run, data=all_res, ylim = c(0,500) )) # Removing this as the sysbench(my version) output does not have response time
 dev.off()
